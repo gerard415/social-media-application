4c89c7cde038a08ccc006b8c5f9a07c31a6aecbe
Chinedum <chinedumike85@gmail.com>
2023-01-05 17:54:02 +0100

    creating the token using jwt and mongoose instance methods

--- backend/models/User.js
+++ backend/models/User.js
@@ -49,6 +49,8 @@ UserSchema.pre('save', async function(){
     this.password = await bcrypt.hash(this.password, salt)
 })
 
-
+UserSchema.methods.createJWT = function(){
+    return jwt.sign({name: this.firstName, userId: this._id}, process.env.JWT_SECRET, {expiresIn: process.env.JWT_LIFETIME})
+}
 
 module.exports = mongoose.model('User', UserSchema)
